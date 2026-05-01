6c54dfacecd1da0c8dab5098c51d9bca9933a27d
mvs123331 <mvs123331@gmail.com>
2026-05-01 16:35:38 +0400

    Initial commit

---
+++ src/test/java/org/neoflex/crudservice/CrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neoflex.crudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
